5106f8e755ca96b7328fee191f16e656149d7772
bgnacinski <borysgnacinski@gmail.com>
2024-06-03 22:17:28 +0200

    changed css

--- app/Views/templates/primary.php
+++ app/Views/templates/primary.php
@@ -17,6 +17,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Gabarito:wght@400..900&display=swap" rel="stylesheet">
 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+
     <?= $this->renderSection("links"); ?>
 </head>
 <body>
